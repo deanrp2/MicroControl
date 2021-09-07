573381257c50301398797cacbddd55a8f2d33dd5
deanrp <deanrp@highpoint.engin.umich.edu>
2021-09-06 23:07:37 -0400

    finished get jminus function

--- reactivity_model.py
+++ reactivity_model.py
@@ -5,11 +5,29 @@ import matplotlib.pyplot as plt
 
 def get_jminus(typ = "wtd"): #wtd, refl or abs
     """Retrieve j- function from model data files."""
+    #pull and load data
     configA_path = Path("model_data/configA_flux_%s_b.csv"%typ)
-    configB_path = Path("model_data/configA_flux_%s_b.csv"%typ)
+    configB_path = Path("model_data/configB_flux_%s_b.csv"%typ)
     jm_configA = pd.read_csv(configA_path, index_col = 0)
     jm_configB = pd.read_csv(configB_path, index_col = 0)
+
+    #adjust 0 angle to be inward to the reactor
+    jm_configA["centers"] -= 3.6820
+    jm_configB["centers"] -= 4.0677
+
+    #center periodic functions on 0
+    jm_configA["centers"][jm_configA["centers"] < -np.pi] += 2*np.pi
+    jm_configB["centers"][jm_configB["centers"] < -np.pi] += 2*np.pi
+
+    #reorder from -pi to pi
+    jm_configA = jm_configA.sort_values(by="centers")
+    jm_configB = jm_configB.sort_values(by="centers")
+
     return jm_configA, jm_configB
+
+def integratePP(x, y, lbnd, ubnd):
+    """Integrate 2*pi periodic, peicewise function given x & y across the 
+    bounds"""
 
 
 
@@ -19,4 +37,8 @@ def get_jminus(typ = "wtd"): #wtd, refl or abs
 #        return drum_reactivity(pert) - drum_reactivity(nom)
 
 if __name__ == "__main__":
-    get_jminus()
+    jmA, jmB = get_jminus("wtd")
+
+    plt.plot(jmA["centers"], jmA["hist"])
+    plt.plot(jmB["centers"], jmB["hist"])
+    plt.show()
