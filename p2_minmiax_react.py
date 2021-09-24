7df1febc8adbbc026026ff6a6b5cbaf24672b714
deanrp <deanrp@highpoint.engin.umich.edu>
2021-09-23 20:00:17 -0400

    made FNEAT algorithm more comprable

--- p2_minmiax_react.py
+++ p2_minmiax_react.py
@@ -22,16 +22,16 @@ def fitness(x):
 #set up bounds
 BOUNDS = {"x%i"%i : ["float", -1.1*np.pi, 1.1*np.pi] for i in range(1, 9)}
 
-#optimize MIN
+#optimize MIN/MAX
 #  Differential evolution
-#de = DE(mode = m, bounds = BOUNDS, fit = fitness, npop = 150,
-#        CR = 0.5, F = 0.7, ncores = 3, verbose = True)
-#de_x, de_y, de_hist = de.evolute(ngen = 60)
-#
-##  Evolution strategies
-#es = ES(mode = m, bounds = BOUNDS, fit = fitness, lambda_ = 140,
-#        mu = 30, ncores = 3)
-#es_x, es_y, es_hist = de.evolute(ngen = 60)
+de = DE(mode = m, bounds = BOUNDS, fit = fitness, npop = 70,
+        CR = 0.5, F = 0.7, ncores = 1, verbose = True)
+de_x, de_y, de_hist = de.evolute(ngen = 60)
+
+#  Evolution strategies
+es = ES(mode = m, bounds = BOUNDS, fit = fitness, lambda_ = 70,
+        mu = 30, ncores = 1)
+es_x, es_y, es_hist = de.evolute(ngen = 60)
 
 #  FNEAT
 config = {"pop_size" : 75,
@@ -41,12 +41,11 @@ config = {"pop_size" : 75,
 fneat = FNEAT(mode = m, bounds = BOUNDS, fit = fitness, config = config,
         ncores = 1)
 x0 = np.random.uniform(-np.pi, np.pi, 8)
-fneat_x, fneat_y, fneat_hist = fneat.evolute(ngen = 5, x0 = x0, startpoint = None,
-        checkpoint_itv=None, verbose = True)
-
-#plt.plot(np.array(es_hist)*1e5, "--k", label = "ES")
-#plt.plot(np.array(de_hist)*1e5, "--r", label = "DE")
-plt.plot(fneat_hist["global_fitness"], "--r", label = "FNEAT")
+fneat_x, fneat_y, fneat_hist = fneat.evolute(ngen = 1, x0 = x0, startpoint = None,
+        checkpoint_itv=None)
+plt.plot(np.array(es_hist)*1e5, "--k", label = "ES")
+plt.plot(np.array(de_hist)*1e5, "--r", label = "DE")
+plt.plot(np.array(fneat_hist["global_fitness"])*1e5, "--b", label = "FNEAT")
 plt.xlabel("Iterations")
 plt.ylabel("reactivity [pcm]")
 plt.legend()
