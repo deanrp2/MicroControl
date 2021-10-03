183440b09586eaa3d85ff4372e1e20d2e2bc0e10
deanrp <deanrp@highpoint.engin.umich.edu>
2021-10-03 18:50:33 -0400

    changed to rerun big sets

--- p5_workspace/es_hist.py
+++ p5_workspace/es_hist.py
@@ -23,7 +23,7 @@ from p5_base import rid, make_objs, calc_cumavg, plot_progress, \
 
 objs = make_objs() #in order react, psplits, dist
 
-a1 = 0.86
+a1 = 0.8
 wts = [a1, 1-a1 - 0.01, .01]
 
 BOUNDS = {"x%i"%i : ["float", -1.1*np.pi, 1.1*np.pi] for i in range(1, 8)}
@@ -35,13 +35,15 @@ mutpb = 0.3
 notes_str = "lambda=%i, mu=%i, cxpb=%f, mutpb=%f\n"%(lambda_, mu, 
         cxpb, mutpb)
 
-#histname = "log/hist0p9"
-histname = "log/hist0p86"
+histname = "log/hist0p8"
 
 rlist = []
+qsplit = []
+dist = []
+
 objectives = []
 drumangles = []
-I = 1000
+I = 2000
 for i in range(I):
     print(i+1, "/", I)
     fname = Path("log/es_%s.log"%rid())
@@ -53,21 +55,14 @@ for i in range(I):
     res = get_log(fname)
     bi = np.argmin(res["fitness"].values)
     rlist.append(res["react_err_obj"].values[bi]*1e5)
+    qsplit.append(res["psplit_err_obj"].values[bi])
+    dist.append(res["tdist_obj"].values[bi]*180/np.pi)
     objectives.append(res["fitness"].values[bi])
     drumangles.append(res.values[bi, 1:8]*180/np.pi)
     with open(histname + ".dat", "a") as f:
-        f.write("%.3f,%.6f"%(rlist[-1], objectives[-1]))
+        f.write("%.3f,%.5f,%.3f,%.6f"%(rlist[-1], qsplit[-1], dist[-1],
+            objectives[-1]))
         for ii in range(7):
             f.write(",%.3f"%drumangles[-1][ii])
         f.write("\n")
 
-#plt.hist(rlist, bins= "auto", color = "#0504aa", rwidth = 0.85)
-#plt.xlabel("Reactivity Error")
-#plt.ylabel("Freq")
-#plt.show()
-#print(np.asarray(es_x)*180/np.pi)
-#print(es_y)
-
-#plot_progress(res["fitness"], lambda_)
-#plot_objs(res)
-#plt.show()
