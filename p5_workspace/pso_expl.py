616afc93879ee76a13b32766579e58a1df31fc30
Dean Price <dean1357price1357@gmail.com>
2021-10-23 20:06:26 -0400

    added opt parms

--- p5_workspace/pso_expl.py
+++ p5_workspace/pso_expl.py
@@ -22,8 +22,8 @@ BOUNDS = {"x%i"%i : ["float", -1.1*np.pi, 1.1*np.pi] for i in range(1, 8)}
 
 npar = 50
 c1 = 2.05
-c2 = 2.05
-speed_mech = "timew"
+c2 = 2.15
+speed_mech = "constric"
 notes_str = "npar=%i, c1=%f, c2=%f, speed_mech=%s\n"%(npar, c1, c2, speed_mech)
 pso_helper = FitnessHelper(objs, wts, fname, notes = notes_str)
 pso = PSO(mode="min", bounds = BOUNDS, fit = pso_helper.fitness,
