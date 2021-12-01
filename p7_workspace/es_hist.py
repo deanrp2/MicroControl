7e12d6d5cd6b60cf3d2250e32634f715c76d16f6
Dean Price <dean1357price1357@gmail.com>
2021-11-30 20:37:25 -0500

    updated with new runs for publication data

--- p7_workspace/es_hist.py
+++ p7_workspace/es_hist.py
@@ -19,7 +19,7 @@ from p7_base import rid, make_objs, calc_cumavg, plot_progress, \
 
 objs = make_objs() #in order react, psplits, diff_worth
 
-wts = [.4, .2, .4]
+wts = [.6, .2, .2]
 
 BOUNDS = {"x%i"%i : ["float", -1.1*np.pi, 1.1*np.pi] for i in range(1, 9)}
 
@@ -30,7 +30,7 @@ mutpb = 0.3
 notes_str = "lambda=%i, mu=%i, cxpb=%f, mutpb=%f\n"%(lambda_, mu, 
         cxpb, mutpb)
 
-histname = "log/hist_p4_p2_p4"
+histname = "log/hist_p6p2p2"
 
 rlist = []
 qsplit = []
@@ -45,7 +45,7 @@ for i in range(I):
     es_helper = FitnessHelper(objs, wts, fname, notes = notes_str)
     es = ES(mode="min", bounds = BOUNDS, fit = es_helper.fitness,
             ncores=1, lambda_ = lambda_, mu = mu, cxpb = cxpb, mutpb = mutpb)
-    es_x, es_y, es_hist = es.evolute(70)
+    es_x, es_y, es_hist = es.evolute(300)
 
     res = get_log(fname)
     bi = np.argmin(res["fitness"].values)
