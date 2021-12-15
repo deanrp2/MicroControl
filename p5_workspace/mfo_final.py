04f300b4ab5aa5aa1c03d03a96aa2c8e2583bf9b
Dean Price <dean1357price1357@gmail.com>
2021-12-15 18:50:08 -0500

    final opt to be verified with serp

---
+++ p5_workspace/mfo_final.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(4)
+
+from mfo_expl import mfo_expl
+
+import sys
+
+sys.path.append("..")
+
+x, y, hist, res, _ = mfo_expl(10000, seed = 5)
+
+opt = res["fitness"].argmin()
+b = res.iloc[opt]
+
+for i in range(1, 8):
+    print(b["x" + str(i)])
+
+print("Reactivity Error:", b["react_err_obj"])
+print("Psplit Error:", b["psplit_err_obj"])
+print("Travel Dist:", b["tdist_obj"])
+
+from reactivity_model import ReactivityModel
+from qpower_model import QPowerModel
+
+print("Basic checks:")
+thetas = np.array([b["x1"], 0] + [b["x"+str(d)] for d in range(2, 8)])
+a = ReactivityModel()
+b = QPowerModel()
+print("Injected Reactivity:", a.eval(thetas))
+print("Target Reactivity:", 0.03072)
+print("Reactivity Error", a.eval(thetas) - 0.03072)
+print("Qpower:", b.eval(thetas))
+print("Th. Psplit Error", np.sum(np.abs(b.eval(thetas)-0.25)))
+print("Travel dist:", np.abs(thetas).max())
