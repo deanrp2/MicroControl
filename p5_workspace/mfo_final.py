dcdbd1ca57437ab9decca085b0931acf3a9f5502
Dean Price <dean1357price1357@gmail.com>
2021-12-19 21:51:46 -0500

    updated what the target reactivity is

--- p5_workspace/mfo_final.py
+++ p5_workspace/mfo_final.py
@@ -9,7 +9,7 @@ import sys
 
 sys.path.append("..")
 
-x, y, hist, res, _ = mfo_expl(300, seed = 5)
+x, y, hist, res, _ = mfo_expl(10000, seed = 5)
 
 opt = res["fitness"].argmin()
 b = res.iloc[opt]
@@ -35,8 +35,8 @@ thetas = np.array([b["x1"], 0] + [b["x"+str(d)] for d in range(2, 8)])
 a = ReactivityModel()
 b = QPowerModel()
 print("Injected Reactivity:", a.eval(thetas))
-print("Target Reactivity:", 0.03072)
-print("Reactivity Error", a.eval(thetas) - 0.03072)
+print("Target Reactivity:", 0.03308)
+print("Reactivity Error", a.eval(thetas) - 0.03308)
 print("Qpower:", b.eval(thetas))
 print("Th. Psplit Error", np.sum(np.abs(b.eval(thetas)-0.25)))
 print("Travel dist:", np.abs(thetas).max())
