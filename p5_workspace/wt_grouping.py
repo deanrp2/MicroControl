c403f9a5e68fd22bb8f3a68d0aa4da8c5aaaf5fb
Dean Price <dean1357price1357@gmail.com>
2021-12-25 11:02:50 -0500

    changed objectives being analyzed

--- p5_workspace/wt_grouping.py
+++ p5_workspace/wt_grouping.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 fnames = ["hist_p5p4p1.dat", "hist_p6p2p1.dat"]
 wts = [[.5,.4,.1],[.6,.2,.1]]
-ylims = [[0,40],[0,0.008]]
+ylims = [[140,180],[0,0.008]]
 labels = [r"w$_c$=%.2f, w$_ p$=%.2f, w$_ d$=%.2f"%(wt[0], wt[1], wt[2]) for wt in wts]
 
 columns = ["rerr", "psplit", "tdist", "obj"] + ["x%i"%i for i in range(1, 8)]
@@ -21,9 +21,11 @@ ara2 = pd.DataFrame(data2, columns  = columns)
 plt.subplot(grid[0])
 plt.ylim(ylims[1])
 plt.xlim(ylims[0])
-plt.plot(ara1["rerr"], ara1["psplit"], "k.", markersize = 1.2, alpha = .8, label = labels[0])
-plt.plot(ara2["rerr"], ara2["psplit"], "r.", markersize = 1.2, alpha = .8, label = labels[1])
-plt.xlabel(nicenames[0])
+#plt.plot(ara1["rerr"], ara1["psplit"], "k.", markersize = 1.2, alpha = .8, label = labels[0])
+#plt.plot(ara2["rerr"], ara2["psplit"], "r.", markersize = 1.2, alpha = .8, label = labels[1])
+plt.plot(ara1["tdist"], ara1["psplit"], "k.", markersize = 1.8, alpha = .9, label = labels[0])
+plt.plot(ara2["tdist"], ara2["psplit"], "r.", markersize = 1.8, alpha = .9, label = labels[1])
+plt.xlabel(nicenames[2])
 plt.ylabel(nicenames[1])
 
 
