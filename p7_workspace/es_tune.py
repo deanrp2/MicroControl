07a5538c5703f4966576cce95853e0a331ac4fb6
Dean Price <dean1357price1357@gmail.com>
2021-12-26 22:16:47 -0500

    made all algorithm tuners to run

--- p7_workspace/es_tune.py
+++ p7_workspace/es_tune.py
@@ -15,7 +15,7 @@ from p7_base import rid, make_objs, calc_cumavg, plot_progress, \
         plot_objs
 
 #perform tune or just print current results?
-tune = False
+tune = True
 #tuning logfile
 tlog = Path("log/es_tune.dat")
 
