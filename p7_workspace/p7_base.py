08e4247e23a3710e1bd32f1a55d4e9ad62d749a4
Dean Price <dean1357price1357@gmail.com>
2021-12-27 16:16:03 -0500

    remved log scaling from all but reactivity

--- p7_workspace/p7_base.py
+++ p7_workspace/p7_base.py
@@ -119,7 +119,10 @@ def plot_objs(res, ax =  None, c = "k"):
             v = res[titles[i]]*np.pi/180*1e5
         else:
             v = res[titles[i]]
-        ax[i].semilogy(res.index, v, c, linewidth = 1, alpha = .4)
+        if titles[i] == "react_err_obj":
+            ax[i].semilogy(res.index, v, c, linewidth = 1, alpha = .4)
+        else:
+            ax[i].plot(res.index, v, c, linewidth = 1, alpha = .4)
         ax[i].plot(res.index, v.iloc[m], c,
             linewidth = 3)
         ax[i].set_ylabel(ylabs[i])
